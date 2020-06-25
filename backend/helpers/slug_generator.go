5f92d38b8bb50b3c5b5d6a575c63d90ce6697ad0
Tobi <tobi@Tobis-MBP.fritz.box>
2020-06-25 15:49:57 +0200

    Slug generation update

--- backend/helpers/slug_generator.go
+++ backend/helpers/slug_generator.go
@@ -7,10 +7,10 @@ import (
 func GenerateRandomSlug() string{
 	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 
-		b := make([]rune, 5)
-		for i := range b {
-			b[i] = letterRunes[rand.Intn(len(letterRunes))]
-		}
+	b := make([]rune, 5)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
 
 	return string(b)
 }
